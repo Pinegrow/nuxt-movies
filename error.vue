2a5d06627d243b2824387eee6fdb8d07e9971ee5
Akayy <techakayy@gmail.com>
2023-09-21 03:36:44 +1000

    Misc updates

--- error.vue
+++ error.vue
@@ -1,15 +1,17 @@
 <script setup>
-const props = defineProps({
-  error: Object,
-})
+  const props = defineProps({
+    error: Object,
+  })
 
-const message = computed(() => String(props.error?.message || ''))
-const is404 = computed(() => props.error?.statusCode === 404 || message.value?.includes('404'))
-const isDev = process.dev
+  const message = computed(() => String(props.error?.message || ''))
+  const is404 = computed(
+    () => props.error?.statusCode === 404 || message.value?.includes('404')
+  )
+  const isDev = process.dev
 
-function handleError() {
-  return clearError({ redirect: '/' })
-}
+  function handleError() {
+    return clearError({redirect: '/'})
+  }
 </script>
 
 <template>
@@ -19,9 +21,12 @@ function handleError() {
         {{ is404 ? 'This page could not be found' : 'An error occurred' }}
       </div>
       <div text-xl op50>
-        Looks like you've followed a broken link or entered a URL that doesn't exist on this site.
+        Looks like you've followed a broken link or entered a URL that doesn't
+        exist on this site.
       </div>
-      <pre v-if="isDev">{{ error }}</pre>
+      <pre v-if="isDev" style="width: 100%; white-space: normal">{{
+        error
+      }}</pre>
       <button n-link border px4 py1 rounded @click="handleError">
         Go Back
       </button>
