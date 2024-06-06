f27ea598caddf16a040bff1c05a2027b2a9a015b
Akayy <techakayy@gmail.com>
2024-06-06 14:24:31 +1000

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,81 @@
+const isDev = process.env.NODE_ENV === 'development'
+
+// const apiBaseUrl = 'http://localhost:3001'
+const apiBaseUrl = 'https://movies-proxy.vercel.app'
+
+export default defineNuxtConfig({
+  modules: [
+    '@pinegrow/nuxt-module',
+    '@vueuse/nuxt',
+    '@unocss/nuxt',
+    '@pinia/nuxt',
+    '@nuxt/image-edge',
+    '@nuxtjs/i18n',
+  ],
+  pinegrow: {
+    liveDesigner: {
+      // ...
+    },
+  },
+  experimental: {
+    inlineSSRStyles: false,
+  },
+  routeRules: {
+    '/**': isDev
+      ? {}
+      : {
+          cache: { swr: true, maxAge: 120, staleMaxAge: 60, headersOnly: true },
+        },
+  },
+  runtimeConfig: {
+    public: {
+      apiBaseUrl,
+    },
+  },
+  devtools: {
+    enabled: true,
+  },
+  image: {
+    provider: 'proxy',
+    providers: {
+      proxy: {
+        provider: 'ipx',
+        options: {
+          baseURL: `${apiBaseUrl}/ipx`,
+        },
+      },
+    },
+  },
+  i18n: {
+    detectBrowserLanguage: {
+      useCookie: false,
+      fallbackLocale: 'en',
+    },
+    strategy: 'no_prefix',
+    locales: [
+      {
+        code: 'en',
+        file: 'en.json',
+      },
+      {
+        code: 'de-DE',
+        file: 'de-DE.json',
+      },
+      {
+        code: 'es-ES',
+        file: 'es-ES.json',
+      },
+      {
+        code: 'ja',
+        file: 'ja.json',
+      },
+      {
+        code: 'zh-CN',
+        file: 'zh-CN.json',
+      },
+    ],
+    lazy: true,
+    langDir: 'internationalization',
+    defaultLocale: 'en',
+  },
+})
